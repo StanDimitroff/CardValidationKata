1e465da6d61c77bfda17b1b4808fdc570b47b170
Stanislav Dimitrov <standimitroff@gmail.com>
2025-03-26 15:31:11 +0200

    create `CardType` enumeration for defining end number of card types

---
+++ CardValidatorCore/CardValidatorCore/CardType.swift
@@ -0,0 +1,16 @@
+//
+//  CardType.swift
+//  CardValidatorCore
+//
+//  Created by Stanislav Dimitrov on 26.03.25.
+//
+
+import Foundation
+
+public enum CardType {
+  case unknown
+  case americanExpress
+  case discover
+  case mastercard
+  case visa
+}
