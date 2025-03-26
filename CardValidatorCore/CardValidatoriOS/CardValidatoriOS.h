bb4b48d252fc508a4a72aa88ce9f72864f1e047e
Stanislav Dimitrov <standimitroff@gmail.com>
2025-03-26 19:08:15 +0200

    Create platform-specific module `CardValidatoriOS` and `ValidateCardTypeView`

---
+++ CardValidatorCore/CardValidatoriOS/CardValidatoriOS.h
@@ -0,0 +1,18 @@
+//
+//  CardValidatoriOS.h
+//  CardValidatoriOS
+//
+//  Created by Stanislav Dimitrov on 26.03.25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardValidatoriOS.
+FOUNDATION_EXPORT double CardValidatoriOSVersionNumber;
+
+//! Project version string for CardValidatoriOS.
+FOUNDATION_EXPORT const unsigned char CardValidatoriOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardValidatoriOS/PublicHeader.h>
+
+
---
+++ CardValidatorCore/CardValidatoriOS/ValidateCardTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  ValidateCardTypeView.swift
+//  CardValidatoriOS
+//
+//  Created by Stanislav Dimitrov on 26.03.25.
+//
+
+import SwiftUI
+
+public struct ValidateCardTypeView: View {
+  public var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+#Preview {
+  ValidateCardTypeView()
+}
