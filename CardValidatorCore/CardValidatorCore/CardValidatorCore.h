f2236abeda2253d8a5b6b1057db0a00462d0c7ce
Stanislav Dimitrov <standimitroff@gmail.com>
2025-03-26 15:28:17 +0200

    create `CardValidatorCore` platform-agnostic module

---
+++ CardValidatorCore/CardValidatorCore/CardValidatorCore.h
@@ -0,0 +1,18 @@
+//
+//  CardValidatorCore.h
+//  CardValidatorCore
+//
+//  Created by Stanislav Dimitrov on 26.03.25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardValidatorCore.
+FOUNDATION_EXPORT double CardValidatorCoreVersionNumber;
+
+//! Project version string for CardValidatorCore.
+FOUNDATION_EXPORT const unsigned char CardValidatorCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardValidatorCore/PublicHeader.h>
+
+
