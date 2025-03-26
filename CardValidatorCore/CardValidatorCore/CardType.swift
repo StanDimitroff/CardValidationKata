1a8a30be9247042a68050f0c172ca8f1e283b555
Stanislav Dimitrov <standimitroff@gmail.com>
2025-03-26 16:28:59 +0200

    describe `regexPattern` for every card type

--- CardValidatorCore/CardValidatorCore/CardType.swift
+++ CardValidatorCore/CardValidatorCore/CardType.swift
@@ -13,4 +13,19 @@ public enum CardType {
   case discover
   case mastercard
   case visa
+
+  var regexPattern: String? {
+    switch self {
+    case .unknown:
+      return nil
+    case .americanExpress:
+      return "^3[47]\\d{13}$"
+    case .discover:
+      return "^65[0-9]{14}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$"
+    case .mastercard:
+      return "^(5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)\\d{12}$"
+    case .visa:
+      return "^4\\d{12}(\\d{3}|\\d{6})?$"
+    }
+  }
 }
